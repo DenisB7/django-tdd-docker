f54c4efe3bd4428a5a462cb07c9ac1a25aa5608c
DenisB7 <denzelbe1987@gmail.com>
2021-07-01 02:57:42 +0500

    check code, test and deploy files and directories

--- app/movies/views.py
+++ app/movies/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status, viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Movie
+from .serializers import MovieSerializer
+
+
+class MoviesViewSet(viewsets.ViewSet):
+    lookup_value_regex = "\d+"
+
+    def list(self, request, format=None):
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, format=None):
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk, format=None):
+        movie = get_object_or_404(Movie, pk=pk)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
